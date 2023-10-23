f48ee56dbc5f0e02d79b7600aab5f6a7f9411b0f
halliec6 <halliec6@gmail.com>
2023-10-22 22:04:21 -0700

    ledgerized

--- src/api/audit.py
+++ src/api/audit.py
@@ -14,37 +14,73 @@ router = APIRouter(
 @router.get("/inventory")
 def get_inventory():
     """ """
-    with db.engine.begin() as connection:
-        ml_result = connection.execute(
-            sqlalchemy.text(
-                """
-                SELECT num_red_ml, num_blue_ml, num_green_ml, gold
-                FROM global_inventory
-                """
-            )
-        )
-    total_ml = 0
-    first_row = ml_result.first()
-    total_ml += first_row.num_red_ml
-    total_ml += first_row.num_green_ml
-    total_ml += first_row.num_blue_ml
-    gold = first_row.gold
+    # with db.engine.begin() as connection:
+    #     ml_result = connection.execute(
+    #         sqlalchemy.text(
+    #             """
+    #             SELECT num_red_ml, num_blue_ml, num_green_ml, gold
+    #             FROM global_inventory
+    #             """
+    #         )
+    #     )
+    # total_ml = 0
+    # first_row = ml_result.first()
+    # total_ml += first_row.num_red_ml
+    # total_ml += first_row.num_green_ml
+    # total_ml += first_row.num_blue_ml
+    # gold = first_row.gold
+
+    # with db.engine.begin() as connection:
+    #     result = connection.execute(
+    #         sqlalchemy.text(
+    #             """
+    #             SELECT quantity
+    #             FROM catalog
+    #             WHERE quantity>0
+    #             """
+    #         )
+    #     )
+    #     potions = 0
+    #     for item in result:
+    #         potions += item.quantity
 
     with db.engine.begin() as connection:
-        result = connection.execute(
+        gold = connection.execute(
             sqlalchemy.text(
                 """
-                SELECT quantity
-                FROM catalog
-                WHERE quantity>0
+                SELECT sum(charge)
+                FROM gold_ledger
+
                 """
             )
         )
-        potions = 0
-        for item in result:
-            potions += item.quantity
+        gold = gold.first()[0]
+
+        potions = connection.execute(
+            sqlalchemy.text(
+                """
+                SELECT sum(quantity)
+                FROM potion_ledger
+                """
+            )
+        )
+        potions = potions.first()[0]
+
+        ml = connection.execute(
+            sqlalchemy.text(
+                """
+                SELECT sum(red_ml) + sum(blue_ml) + sum(green_ml)
+                FROM ml_ledger
+
+                """
+            )
+        )
+        total_ml = ml.first()[0]
+        
    
     #need to add up all the ml, gold and potions
+    print("audit returning")
+    print("number_of_potions: ", potions, "total_ml: ", total_ml, "gold: ", gold)
     return {"number_of_potions": potions, "ml_in_barrels": total_ml, "gold": gold}
 
 class Result(BaseModel):
