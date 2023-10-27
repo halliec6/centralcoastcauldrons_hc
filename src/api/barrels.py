abc81a061d12012cc541307f6ca3cf17e61a76d7
halliec6 <halliec6@gmail.com>
2023-10-26 17:01:01 -0700

    barrels plan updated

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -49,83 +49,120 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         num_gold = result.first()[0]
         #gold = first_row.result
 
-    print("Total gold:", num_gold)
-   
+        print("Total gold:", num_gold)
+        result = connection.execute(sqlalchemy.text(
+            """
+            SELECT sum(red_ml)
+            FROM ml_ledger
+            """
+        ))
+        num_red_ml = result.first()[0]
+        
+        result = connection.execute(sqlalchemy.text(
+            """
+            SELECT sum(green_ml)
+            FROM ml_ledger
+            """
+        ))
+        num_green_ml = result.first()[0]
+
+        result = connection.execute(sqlalchemy.text(
+            """
+            SELECT sum(blue_ml)
+            FROM ml_ledger
+            """
+        ))
+        num_blue_ml= result.first()[0]
+    
+    
+    
     red_purchase = 0
     green_purchase = 0
     blue_purchase = 0 
 
     print("data pulled from server:")
     print("gold: ", num_gold)
-   
-    #turn part w/in the if into a func
-    for barrel in wholesale_catalog:
-        if barrel.sku == 'SMALL_RED_BARREL':
-            #then calculate how many to purchase
-            available = barrel.quantity
+    print("red_ml: ", num_red_ml)
+    print("green_ml: ", num_green_ml)
+    print("blue_ml: ", num_blue_ml)
+    
+    hashmap = {}
+    
+    hashmap["SMALL_RED_BARREL"]={"quantity": 0} 
+    hashmap["SMALL_GREEN_BARREL"]={"quantity": 0} 
+    hashmap["SMALL_BLUE_BARREL"]={"quantity": 0} 
 
-            red_purchase = num_gold//barrel.price
-            
-            #can only buy what is available
-            if red_purchase>available:
-                red_purchase = available 
+    bought = True
+    #need to update this logic to be more efficient
+    while num_gold >=100 and bought == True:    
+        bought = False
+        for barrel in wholesale_catalog:
+            if barrel.sku == 'SMALL_RED_BARREL' and num_red_ml<= num_green_ml and num_red_ml <= num_blue_ml:
+                available = barrel.quantity
+
+                red_purchase = num_gold//barrel.price
+                if red_purchase>available:
+                        red_purchase = available 
+                if red_purchase>0:    
+                    num_gold = num_gold - (barrel.price * red_purchase)
+                    barrel.quantity = barrel.quantity - red_purchase
+                    bought = True
+                    hashmap["SMALL_RED_BARREL"]["quantity"] = hashmap["SMALL_RED_BARREL"]["quantity"] + red_purchase
                 
-            #want to buy one of ea so I don't blow my money all in one place
-            if red_purchase >= 1:
-                red_purchase = 1
+            elif barrel.sku == 'SMALL_GREEN_BARREL' and num_green_ml <= num_red_ml and num_green_ml<= num_blue_ml:
                 
-                #needs to change once buying more than one barrel
-                num_gold = num_gold - barrel.price        
-        elif barrel.sku == 'SMALL_GREEN_BARREL':
-            
-            available = barrel.quantity
+                available = barrel.quantity
 
-            green_purchase = num_gold//barrel.price 
-            
-            #can only buy what is available
-            if green_purchase>available:
-                green_purchase = available 
+                green_purchase = num_gold//barrel.price 
+                
+                if green_purchase>available:
+                        green_purchase = available 
+                if green_purchase>0: 
+                    num_gold = num_gold - (barrel.price * green_purchase)
+                    barrel.quantity = barrel.quantity - green_purchase
+                    bought = True
+                    hashmap["SMALL_GREEN_BARREL"]["quantity"] = hashmap["SMALL_GREEN_BARREL"]["quantity"] + green_purchase
 
-            if green_purchase >=1:
-                green_purchase = 1            
-                num_gold = num_gold - barrel.price
 
-        elif barrel.sku == 'SMALL_BLUE_BARREL':
-            available = barrel.quantity
+            elif barrel.sku == 'SMALL_BLUE_BARREL':
+                available = barrel.quantity
 
-            blue_purchase = num_gold//barrel.price 
-            
-            #can only buy what is available
-            if blue_purchase>available:
-                blue_purchase = available 
+                blue_purchase = num_gold//barrel.price 
+                
+                if blue_purchase>available:
+                        blue_purchase = available 
+                
+                if blue_purchase>0:                        
+                    num_gold = num_gold - (barrel.price * blue_purchase)
+                    barrel.quantity = barrel.quantity - blue_purchase
+                    bought = True
+                    hashmap["SMALL_BLUE_BARREL"]["quantity"] = hashmap["SMALL_BLUE_BARREL"]["quantity"] + blue_purchase
 
-            if blue_purchase >=1:
-                blue_purchase = 1
-                num_gold = num_gold - barrel.price
+                
 
     print("\nreturn values:")
-    print("- sku: SMALL_RED_BARREL    Quantity: ", red_purchase)
-    print("- sku: SMALL_GREEN_BARREL  Quantity: ", green_purchase)
-    print("- sku: SMALL_BLUE_BARREL   Quantity: ", blue_purchase)
-
+    print("- sku: SMALL_RED_BARREL    Quantity: ", hashmap["SMALL_RED_BARREL"]["quantity"])
+    print("- sku: SMALL_GREEN_BARREL  Quantity: ", hashmap["SMALL_GREEN_BARREL"]["quantity"])
+    print("- sku: SMALL_BLUE_BARREL   Quantity: ", hashmap["SMALL_BLUE_BARREL"]["quantity"])
+    print("gold remaining: ", num_gold)
 
     ans = []
 
-    if red_purchase>0:
+    if hashmap["SMALL_RED_BARREL"]["quantity"]>0:
         ans.append({
             "sku": "SMALL_RED_BARREL",
-            "quantity": red_purchase,
+            "quantity": hashmap["SMALL_RED_BARREL"]["quantity"],
         })
-    if green_purchase>0:
+    if hashmap["SMALL_GREEN_BARREL"]["quantity"]>0:
         ans.append({
             "sku": "SMALL_GREEN_BARREL",
-            "quantity": green_purchase
+            "quantity": hashmap["SMALL_GREEN_BARREL"]["quantity"]
         })
-    if blue_purchase>0:
+    if hashmap["SMALL_BLUE_BARREL"]["quantity"]>0:
         ans.append(
         {
             "sku": "SMALL_BLUE_BARREL",
-            "quantity": blue_purchase
+            "quantity": hashmap["SMALL_BLUE_BARREL"]["quantity"]
         }
         )
     print("return value of bbarrels plan: ", ans)
