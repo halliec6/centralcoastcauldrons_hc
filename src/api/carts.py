3197557bdcfbae7c29aac290fe949d1824e58a4e
halliec6 <halliec6@gmail.com>
2023-10-28 11:21:05 -0700

    carts w search endpoint (empty)

--- src/api/carts.py
+++ src/api/carts.py
@@ -3,19 +3,73 @@ from pydantic import BaseModel
 from src.api import auth
 import sqlalchemy
 from src import database as db
-
+from enum import Enum
 
 router = APIRouter(
     prefix="/carts",
     tags=["cart"],
     dependencies=[Depends(auth.get_api_key)],
 )
+class search_sort_options(str, Enum):
+    customer_name = "customer_name"
+    item_sku = "item_sku"
+    line_item_total = "line_item_total"
+    timestamp = "timestamp"
 
-#how is this useful?? - this is not useful to me right now, will be useful later
+class search_sort_order(str, Enum):
+    asc = "asc"
+    desc = "desc"   
+
+@router.get("/search/", tags=["search"])
+def search_orders(
+    customer_name: str = "",
+    potion_sku: str = "",
+    search_page: str = "",
+    sort_col: search_sort_options = search_sort_options.timestamp,
+    sort_order: search_sort_order = search_sort_order.desc,
+):
+    """
+    Search for cart line items by customer name and/or potion sku.
+
+    Customer name and potion sku filter to orders that contain the 
+    string (case insensitive). If the filters aren't provided, no
+    filtering occurs on the respective search term.
+
+    Search page is a cursor for pagination. The response to this
+    search endpoint will return previous or next if there is a
+    previous or next page of results available. The token passed
+    in that search response can be passed in the next search request
+    as search page to get that page of results.
+
+    Sort col is which column to sort by and sort order is the direction
+    of the search. They default to searching by timestamp of the order
+    in descending order.
+
+    The response itself contains a previous and next page token (if
+    such pages exist) and the results as an array of line items. Each
+    line item contains the line item id (must be unique), item sku, 
+    customer name, line item total (in gold), and timestamp of the order.
+    Your results must be paginated, the max results you can return at any
+    time is 5 total line items.
+    """
+
+    return {
+        "previous": "",
+        "next": "",
+        "results": [
+            {
+                "line_item_id": 1,
+                "item_sku": "1 oblivion potion",
+                "customer_name": "Scaramouche",
+                "line_item_total": 50,
+                "timestamp": "2021-01-01T00:00:00Z",
+            }
+        ],
+    }
+
 class NewCart(BaseModel):
     customer: str
 
-#store given data, return ID
 @router.post("/")
 def create_cart(new_cart: NewCart):
     """ """
@@ -34,11 +88,8 @@ def create_cart(new_cart: NewCart):
             [{"new_cart_str": new_cart.customer}])
     first_row = id.first()
     id = first_row.cart_id
-    #print(id)
     return {"cart_id": id}
 
-
-#return the cart information
 @router.get("/{cart_id}")
 def get_cart(cart_id: int):
     """ """
@@ -50,7 +101,6 @@ def get_cart(cart_id: int):
 class CartItem(BaseModel):
     quantity: int
 
-#get cart, add item to cart if it is available in the database
 @router.post("/{cart_id}/items/{item_sku}")
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ """
@@ -73,7 +123,6 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
 class CartCheckout(BaseModel):
     payment: str
 
-#update database, have to have enough for customer, transaction is all or nothing (check what payment looks like in logs?)
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
@@ -81,9 +130,7 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     print("cart_id: ", cart_id, "cart_checkout: ", cart_checkout)
         
     with db.engine.begin() as connection:
-        #create a transaction:
         transaction_id = connection.execute(
-            #add the transaction statement first and track the transaction ID ---> need to
             sqlalchemy.text(
                 """
                 INSERT INTO transactions (description, tag)
